f1a8d633fe26155833c26109e37beeef0bf803d4
kaustav-goswami <kggoswami@ucdavis.edu>
2023-04-25 14:03:28 -0700

    files: added the solution
    Signed-off-by: kaustav-goswami <kggoswami@ucdavis.edu>

--- src/main/scala/components/alucontrol.scala
+++ src/main/scala/components/alucontrol.scala
@@ -34,6 +34,47 @@ class ALUControl extends Module {
 
   io.operation := "b11111".U // Invalid
 
-  // Your code goes here
+  when (io.aluop === 1.U) { // 64-bit R-type
+    io.operation := MuxCase(
+      "b11111".U, // default: Invalid
+      Array(
+        ((io.funct3 === "b111".U) & (io.funct7 === "b0000000".U)) -> "b01101".U, // and
+        ((io.funct3 === "b000".U) & (io.funct7 === "b0100000".U)) -> "b00010".U, // sub
+        ((io.funct3 === "b000".U) & (io.funct7 === "b0000001".U)) -> "b00101".U, // mul
+        ((io.funct3 === "b001".U) & (io.funct7 === "b0000000".U)) -> "b10010".U, // sll
+        ((io.funct3 === "b001".U) & (io.funct7 === "b0000001".U)) -> "b00111".U, // mulh
+        ((io.funct3 === "b010".U) & (io.funct7 === "b0000000".U)) -> "b10110".U, // slt
+        ((io.funct3 === "b010".U) & (io.funct7 === "b0000001".U)) -> "b11000".U, // mulhsu
+        ((io.funct3 === "b011".U) & (io.funct7 === "b0000000".U)) -> "b10111".U, // sltu
+        ((io.funct3 === "b011".U) & (io.funct7 === "b0000001".U)) -> "b01000".U, // mulhu
+        ((io.funct3 === "b100".U) & (io.funct7 === "b0000000".U)) -> "b01111".U, // xor
+        ((io.funct3 === "b100".U) & (io.funct7 === "b0000001".U)) -> "b01001".U, // div
+        ((io.funct3 === "b101".U) & (io.funct7 === "b0000000".U)) -> "b10100".U, // srl
+        ((io.funct3 === "b101".U) & (io.funct7 === "b0100000".U)) -> "b10000".U, // sra
+        ((io.funct3 === "b101".U) & (io.funct7 === "b0000001".U)) -> "b01010".U, // divu
+        ((io.funct3 === "b110".U) & (io.funct7 === "b0000000".U)) -> "b01110".U, // or
+        ((io.funct3 === "b110".U) & (io.funct7 === "b0000001".U)) -> "b11100".U, // rem
+        ((io.funct3 === "b000".U) & (io.funct7 === "b0000000".U)) -> "b00000".U, // add
+        ((io.funct3 === "b111".U) & (io.funct7 === "b0000001".U)) -> "b11011".U  // remu
+      )
+    )
+  }
+  .elsewhen (io.aluop === 3.U) { // 32-bit R-type
+    io.operation := MuxCase(
+      "b11111".U, // default: Invalid
+      Array(
+        ((io.funct3 === "b000".U) & (io.funct7 === "b0000000".U)) -> "b00001".U, // addw
+        ((io.funct3 === "b000".U) & (io.funct7 === "b0100000".U)) -> "b00100".U, // subw
+        ((io.funct3 === "b000".U) & (io.funct7 === "b0000001".U)) -> "b00110".U, // mulw
+        ((io.funct3 === "b001".U) & (io.funct7 === "b0000000".U)) -> "b10011".U, // sllw
+        ((io.funct3 === "b100".U) & (io.funct7 === "b0000001".U)) -> "b01011".U, // divw
+        ((io.funct3 === "b101".U) & (io.funct7 === "b0000000".U)) -> "b10101".U, // srlw
+        ((io.funct3 === "b101".U) & (io.funct7 === "b0100000".U)) -> "b10001".U, // sraw
+        ((io.funct3 === "b101".U) & (io.funct7 === "b0000001".U)) -> "b01100".U, // divuw
+        ((io.funct3 === "b110".U) & (io.funct7 === "b0000001".U)) -> "b11010".U, // remw
+        ((io.funct3 === "b111".U) & (io.funct7 === "b0000001".U)) -> "b11001".U  // remuw
+      )
+    )
+  }
 
 }
